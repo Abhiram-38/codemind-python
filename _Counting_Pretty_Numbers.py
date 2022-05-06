ec986d8dba7754f9dc97b9f5406faccff7d05f9a
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-05-06 17:25:59 +0530

    t=int(input()) while t:     a,b=map(int,input().split())     c=0     for i in range(a,b+1):         d=i%10         if(d==2 or d==3 or d==9):             c+=1     print(c)     t-=1

---
+++ _Counting_Pretty_Numbers.py
@@ -0,0 +1,10 @@
+t=int(input())
+while t:
+    a,b=map(int,input().split())
+    c=0
+    for i in range(a,b+1):
+        d=i%10
+        if(d==2 or d==3 or d==9):
+            c+=1
+    print(c)
+    t-=1
