59a0492491da2463b4faf765a5af12b69752ad71
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-06-07 22:26:54 +0530

    n=input() n=n.split() m=0 for i in n:     c=0     for j in i:         if j=='a' or j=='e' or j=='i' or j=='o' or j=='u' or j=='A' or j=='E' or j=='I' or j=='O' or j=='U':             c+=1     if c>m:         m=c print(m)

---
+++ maximum_vowel_count.py
@@ -0,0 +1,11 @@
+n=input()
+n=n.split()
+m=0
+for i in n:
+    c=0
+    for j in i:
+        if j=='a' or j=='e' or j=='i' or j=='o' or j=='u' or j=='A' or j=='E' or j=='I' or j=='O' or j=='U':
+            c+=1
+    if c>m:
+        m=c
+print(m)
