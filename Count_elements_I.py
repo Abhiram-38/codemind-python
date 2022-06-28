8fb4435852bcf8711847a24eaaee9b8b38abb925
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-06-28 21:00:40 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) a=set(a) b=set(b) c=a.intersection(b) print(len(c))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,10 +1,7 @@
-m,n=map(int,input().split())
+n,m=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
-c=0
 a=set(a)
 b=set(b)
-for i in a:
-    if i in b:
-        c+=1
-print(c)
+c=a.intersection(b)
+print(len(c))
