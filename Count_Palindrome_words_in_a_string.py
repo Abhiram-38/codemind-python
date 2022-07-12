180a0428420ca758af1a3fff093b1d13a9cb595a
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-07-12 23:06:30 +0530

    s=input() s=s.lower() s=s.split(' ') c=0 for i in range(len(s)):     if s[i]==s[i][::-1]:         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,8 +1,8 @@
-n=input()
-n=n.split()
+s=input()
+s=s.lower()
+s=s.split(' ')
 c=0
-for i in n:
-    i=i.lower()
-    if i==i[::-1]:
+for i in range(len(s)):
+    if s[i]==s[i][::-1]:
         c+=1
 print(c)
