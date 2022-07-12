996ce56336500fab1e0833d20f0c6462833f32b8
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-07-12 22:43:32 +0530

    s=input() s=list(s) a=[] c=0 for i in s:     a.append(ord(i)) for i in a:     if (i>=65 and i<=90) or (i>=97 and i<=122):         continue     elif i==32:         continue     else:         c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,11 +1,13 @@
-n=input()
+s=input()
+s=list(s)
+a=[]
 c=0
-for i in n:
-    if i.isalpha():
+for i in s:
+    a.append(ord(i))
+for i in a:
+    if (i>=65 and i<=90) or (i>=97 and i<=122):
         continue
-    elif i.isdigit():
-        continue
-    elif i==' ':
+    elif i==32:
         continue
     else:
         c+=1
