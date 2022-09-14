dfff173dbc51378384b0a0d69bbdd52377da01a1
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-09-15 00:02:49 +0530

    def fun(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) if fun(n):     print(0) else:     n1=n+1     n2=n-1     while True:         if fun(n1):             x=n1             break         n1+=1     while True:         if fun(n2):             y=n2             break         n2-=1     if abs(n1-n)>abs(n2-n):         print(abs(n2-n))     elif abs(n1-n)<abs(n2-n):         print(abs(n1-n))     else:         print(abs(n1-n))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,25 +1,30 @@
-n=int(input())
-def prime(n):
-    for i in range(2,int(n**0.5)+1,1):
+def fun(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
-            return False
-            break
+            return 0
     else:
-        return True
-if prime(n):
-    print("0")
+        return 1
+n=int(input())
+if fun(n):
+    print(0)
 else:
-    i=n+1
+    n1=n+1
+    n2=n-1
     while True:
-        if prime(i):
-            np=i
+        if fun(n1):
+            x=n1
             break
-        i+=1
-    for i in range(n-1,0,-1):
-        if prime(i):
-            pp=i
+        n1+=1
+    while True:
+        if fun(n2):
+            y=n2
             break
-    if n-pp<np-n:
-        print(n-pp)
+        n2-=1
+    if abs(n1-n)>abs(n2-n):
+        print(abs(n2-n))
+    elif abs(n1-n)<abs(n2-n):
+        print(abs(n1-n))
     else:
-        print(np-n)
+        print(abs(n1-n))
