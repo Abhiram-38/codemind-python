e6ce99e02ccbb5180ece8cd5b30ef530ded5356a
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-09-15 00:03:55 +0530

    def fun1(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) if fun1(n):     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,13 @@
+def fun1(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    else:
+        return 1
 n=int(input())
-fc=0
-for i in range(1,n+1):
-    if n%i==0:
-        fc+=1
-if(fc==2):
-    print('prime')
+if fun1(n):
+    print("prime")
 else:
-    print('not a prime')
+    print("not a prime")
