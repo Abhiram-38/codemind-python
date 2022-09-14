1f9991754f8071c810c1e646f5ded21d3e10231b
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-09-15 00:47:00 +0530

    def fun(n):     r=0     temp=n     while n:         d=n%10         r=r*10+d         n=n//10     if r==temp:         return 1     else:         return 0 n=int(input()) if fun(n):     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,16 @@
+def fun(n):
+    r=0
+    temp=n
+    while n:
+        d=n%10
+        r=r*10+d
+        n=n//10
+    if r==temp:
+        return 1
+    else:
+        return 0
 n=int(input())
-temp=n
-res=0
-while n:
-    d=n%10
-    res=res*10+d
-    n=n//10
-if(temp==res):
-    print('True')
+if fun(n):
+    print(True)
 else:
-    print('False')
+    print(False)
