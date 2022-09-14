11ab93d6e1aa08ee06663e5a1d7c21db3c2fbb2e
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-09-15 00:23:18 +0530

    n=int(input())#12 x=n*n#144 y=0 while n:     d=n%10     y=y*10+d     n=n//10 z=y*y res=0 while z:     d=z%10     res=res*10+d     z=z//10 if x==res:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,17 @@
-n=int(input())
-a=n*n
-b=0
-e=0
+n=int(input())#12
+x=n*n#144
+y=0
 while n:
     d=n%10
-    b=b*10+d
+    y=y*10+d
     n=n//10
-c=b*b
-while c:
-    d=c%10
-    e=e*10+d
-    c=c//10
-if e==a:
-    print('True')
+z=y*y
+res=0
+while z:
+    d=z%10
+    res=res*10+d
+    z=z//10
+if x==res:
+    print(True)
 else:
-    print('False')
+    print(False)
