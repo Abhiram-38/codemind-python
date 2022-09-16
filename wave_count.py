aa40d40d71de01054328563cef31f6b17abf7d22
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-09-16 23:29:59 +0530

    _=int(input()) v=list(map(int,input().split())) a=b=0 for k in range(0,len(v)-2,2):     b+=1     if(v[k+1]>v[k] and v[k+1]>v[k+2]):         a+=1 if(a==b):     print(a) else:     print(-1)

--- wave_count.py
+++ wave_count.py
@@ -1,11 +1,11 @@
-n=int(input())
-arr=list(map(int,input().split()))
+_=int(input())
+v=list(map(int,input().split()))
 a=b=0
-for i in range(0,n-2,2):
-    a+=1
-    if(arr[i+1]>arr[i] and arr[i+1]>arr[i+2]):
-        b+=1
-if a==b:
+for k in range(0,len(v)-2,2):
+    b+=1
+    if(v[k+1]>v[k] and v[k+1]>v[k+2]):
+        a+=1
+if(a==b):
     print(a)
 else:
     print(-1)
