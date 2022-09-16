0dd6a1e5385fb2c15a3066628b2a3e9b61cd5c28
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-09-16 22:52:31 +0530

    s=input() s=s.lower() s=s.split() x=s[0] y=s[1] a=[] b=[] for i in x:     if i in y:         a.append(i) for i in a:     c=0     for j in s:         if i in j:             c+=1     if c==len(s):         b.append(i) if len(b)==0:     print(-1) else:     print(min(b))

--- Common_characters_-_3.py
+++ Common_characters_-_3.py
@@ -1,14 +1,19 @@
-s=input().lower()
-s1=(s.split(' '))[0]
+s=input()
+s=s.lower()
+s=s.split()
+x=s[0]
+y=s[1]
+a=[]
 b=[]
-for i in s1:
-    a=0
-    for j in s.split(' '):
+for i in x:
+    if i in y:
+        a.append(i)
+for i in a:
+    c=0
+    for j in s:
         if i in j:
-            a+=1
-        else:
-            a=0
-    if a==len(s.split(' ')):
+            c+=1
+    if c==len(s):
         b.append(i)
 if len(b)==0:
     print(-1)
